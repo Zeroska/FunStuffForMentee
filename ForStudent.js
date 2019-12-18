cacdb283d957c7f875e1a16054b941af35a426be
Zeroska <owlthres@gmail.com>
2019-12-18 12:30:50 +0700

    LinkedList using Javascript

--- ForStudent.js
+++ ForStudent.js
@@ -1,8 +1,63 @@
 
 
+function isArmStrong(number){
+	let total = 0
+	let temp = number 
+	while(temp != 0){
+		let digit = temp%10
+		total += digit**3
+		temp = parseInt(temp/10) 
+	}	
+	if (total == i){
+		return true
+	}
+	return false 
+}
 
-<body>
-	<script>
-		console.log("Nguyen Dang Khuong")
-	</script>
-</body>
+
+let result = ""
+let i  = 1
+while(i<999){
+	let kq = isArmStrong(i)
+	if (kq  == true){
+		result += i + " ";
+	}
+	i++
+}
+console.log(result)
+
+
+
+const Node = ()=>{
+	this.data = {} 
+	this.next = null
+}
+
+const LinkedList = ()=>{
+	this.head = null 
+}
+
+
+LinkedList.prototype.addHead = (Node)=>{
+	if(this.head == null){
+		this.head = node 
+		
+	}
+	else{
+		node.next = this.head
+		this.head = node 	
+	}
+}
+
+LinkedList.prototype.printList = ()=>{
+	let current = this.head
+	while(current != null){
+		console.log(current)
+		current = current.next 
+	}
+}
+
+
+
+
+
